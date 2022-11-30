ee5da68cc58f65a4ffbc53e320d81ba6121fcd5d
Thiago Vinícius <thiagovao2000@hotmail.com>
2022-11-30 10:59:24 -0300

    Cria fluxo de login, registro e recuperação de senha, ainda sem enviar o código para o email.

--- src/main/java/br/ufrn/imd/scrum/controller/PersonController.java
+++ src/main/java/br/ufrn/imd/scrum/controller/PersonController.java
@@ -1,14 +1,19 @@
 package br.ufrn.imd.scrum.controller;
 
 import br.ufrn.imd.scrum.model.PersonModel;
+import br.ufrn.imd.scrum.model.dto.MeetingDto;
 import br.ufrn.imd.scrum.model.dto.PersonDto;
 import br.ufrn.imd.scrum.service.PersonService;
 import br.ufrn.imd.springcrud.controller.GenericController;
+import br.ufrn.imd.springcrud.exception.ValidationException;
 import br.ufrn.imd.springcrud.service.GenericService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/people")
@@ -23,5 +28,26 @@ public class PersonController extends GenericController<PersonModel, PersonDto> 
     @Autowired
     public void setPersonService(PersonService personService) {
         this.personService = personService;
+    }
+
+    @GetMapping({"login"})
+    public ResponseEntity<?> login(@RequestParam("name") String name, @RequestParam("password") String password) throws ValidationException {
+        return ResponseEntity.ok(this.personService.login(name, password));
+    }
+
+    @PostMapping({"register"})
+    public ResponseEntity<?> register(@RequestParam("name") String name, @RequestParam("password") String password, @RequestParam("email") String email) throws ValidationException {
+        return ResponseEntity.ok(this.personService.register(name, password, email));
+    }
+
+    @PostMapping({"sendCode"})
+    public ResponseEntity<?> sendCode(@RequestParam("email") String email, @RequestParam("code") String code) throws ValidationException {
+        this.personService.sendCode(email, code);
+        return ResponseEntity.ok(code);
+    }
+
+    @PostMapping({"passwordChange"})
+    public ResponseEntity<?> passwordChange(@RequestParam("email") String email, @RequestParam("newPassword") String newPassword) throws ValidationException {
+        return ResponseEntity.ok(this.personService.passwordChange(email, newPassword));
     }
 }
--- src/main/java/br/ufrn/imd/scrum/repository/PersonRepository.java
+++ src/main/java/br/ufrn/imd/scrum/repository/PersonRepository.java
@@ -1,11 +1,18 @@
 package br.ufrn.imd.scrum.repository;
 
+import br.ufrn.imd.scrum.model.MeetingModel;
 import br.ufrn.imd.springcrud.repository.GenericRepository;
 import br.ufrn.imd.scrum.model.PersonModel;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonRepository extends GenericRepository<PersonModel> {
+    public PersonModel findByNickname(String username);
 
+    public PersonModel findByEmail(String username);
 }
